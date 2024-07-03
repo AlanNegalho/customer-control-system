05b0a9e90e35862b13e10c0f689f95fadaa29913
AlanNegalho <alandevads@gmail.com>
2024-07-02 22:16:53 -0300

    create client

--- obj/Debug/net8.0/customer control system.AssemblyInfo.cs
+++ obj/Debug/net8.0/customer control system.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("customer control system")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55f862e47bc50136a07f86a93c7ee82abea523ec")]
 [assembly: System.Reflection.AssemblyProductAttribute("customer control system")]
 [assembly: System.Reflection.AssemblyTitleAttribute("customer control system")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
